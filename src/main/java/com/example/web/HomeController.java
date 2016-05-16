222e2d051d99687a50fb40537eee2fb4842f3def
tindomiel <tindomiel@outlook.com>
2016-05-16 13:47:23 +0900

    first commit

---
+++ src/main/java/com/example/web/HomeController.java
@@ -0,0 +1,23 @@
+/*
+ * @(#)HomeController.java $version 2016. 4. 26.
+ *
+ * Copyright 2007 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("home");
+		mav.addObject("message", "hello!");
+
+		return mav;
+	}
+}
